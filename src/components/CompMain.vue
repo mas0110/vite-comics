1759f8161d932547375566eaf2d5b6424666983a
mas0110 <masoudmohammed13@gmail.com>
2024-02-07 21:09:18 +0100

    main

---
+++ src/components/CompMain.vue
@@ -0,0 +1,63 @@
+<script>
+export default{
+    name: "Main"
+}
+</script>
+
+<template>
+<div class="content">
+    <h3>** Content goes here **</h3>
+</div>
+<div class="buyComics">
+<ul>
+    <li>
+        <img src="../assets/img/buy-comics-digital-comics.png" alt="digital">
+        <span>DIGITAL COMICS</span>
+    </li>
+    <li>
+        <img src="../assets/img/buy-comics-merchandise.png" alt="merchandise">
+        <span>DC MERCHARDISE</span>
+    </li>
+    <li>
+        <img src="../assets/img/buy-comics-subscriptions.png" alt="subscriptions">
+        <span>SUBSCRIPTION</span>
+    </li>
+    <li>
+        <img src="../assets/img/buy-comics-shop-locator.png" alt="location">
+        <span>COMIC SHOP LOCATOR</span>
+    </li>
+    <li>
+        <img src="../assets/img/buy-dc-power-visa.svg" alt="visa">
+        <span>DC POWER VISA</span>
+    </li>
+</ul>
+</div>
+</template>
+
+<style scoped>
+.content{
+    background-color: #1C1C1C;
+    color: white;
+    padding: 25px;
+}
+.buyComics{
+background-color:#0082F9 ;
+padding: 20px;
+color: white;
+position: relative;
+display: flex;
+justify-content: center;
+}
+.buyComics ul{
+    display: flex;
+    gap: 15px;
+}
+.buyComics ul img{
+    height: 45px;
+}
+.buyComics ul li{
+    display: flex;
+    gap: 15px;
+    align-items: center;
+}
+</style>

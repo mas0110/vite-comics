2aa2b09cd2f58b2f0a12f862122ac59d421f79c4
mas0110 <masoudmohammed13@gmail.com>
2024-02-07 21:09:03 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script>
+import CompHeader from './components/CompHeader.vue';
+import CompMain from './components/CompMain.vue';
+import CompFooter from './components/CompFooter.vue';
+
+
+export default{
+  components: {
+    CompHeader,
+    CompMain,
+    CompFooter,
+  }
+}
 
 </script>
 
 <template>
-  
+    <div class="mainContainer">
+        <CompHeader/>
+        <CompMain/>
+        <CompFooter/>
+    </div>
 </template>
 
 <style>
-
+li{
+    list-style: none;
+}
+.mainContainer{
+    height: 100vh;
+}
 </style>
---
+++ src/components/CompHeader.vue
@@ -0,0 +1,56 @@
+<script>
+export default{
+    name: "ciao"
+}
+</script>
+
+<template>
+    <div>
+        <figure>
+           <img src="../assets/img/dc-logo-bg.png" alt="">
+        </figure>
+        <div>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTION</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+div{
+   display: flex;
+   justify-content: space-around;
+   vertical-align: baseline;
+   align-items: center;
+   padding-block: 10px;
+}
+div figure{
+    margin: 0;
+}
+div figure img{
+   height: 80px;
+
+}
+div div ul{
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    margin-top: 10px;
+    color:#573F51 ;
+}
+div div ul li:hover{
+    color: blue;
+    border-bottom: 1px solid blue;
+    padding-bottom: 50px;
+}
+</style>
